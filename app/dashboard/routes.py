7d2d4cf67308385b0ca6ddaba2e570a347215fe7
sdfy-fly <rozhov25@bk.ru>
2024-01-09 12:20:24 +0500

    add dynamic home link

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -7,5 +7,5 @@ dashboard_router = APIRouter()
 
 
 @dashboard_router.get("/", response_class=HTMLResponse)
-async def main(request: Request):
-    return templates.TemplateResponse("home/index.html", {"request": request})
+async def dashboard(request: Request):
+    return templates.TemplateResponse("dashboard/index.html", {"request": request})
